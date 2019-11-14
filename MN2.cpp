b0cb03dba1f33be2edd0dd3de9fa2bb27de73585
dariocostta <dariocostta@gmail.com>
2019-11-13 22:46:49 -0300

    Mudança para Struct - Sistema e função para a Pivotacao Parcial

--- MN2.cpp
+++ MN2.cpp
@@ -7,6 +7,99 @@ using std::cout;
 using std::cin;
 using std::string;
 
+typedef struct def_sistema {
+	double ** A;
+	int m, n;
+	int * permutacaoLinhas, * permutacaoColunas;
+	
+	/* Função gauss
+	 * Acao:
+	 * Algoritmo:
+	 * Entrada:
+	 * Saida padrao: 
+	 * Saida erro:
+	 */
+	void gauss(){
+	  for(int i=0; i < m; i++){
+	    if(A[i][j] == 0){
+	      for(p=0; p < m; p++){
+	        if(A[p][i] != 0){
+	          k=p;
+	          break;
+	        }
+	        for(p=i-1; p < m; p++){
+	          aux=A[i][p];
+	          A[i][p]=A[k][p];
+	           A[k][p]=aux;
+	        }
+	      }
+	      j=0;
+	      while(j < m){
+	        for(k=m-1; k<n; k++){
+	          b = A[i][k];
+	          A[j][k] = A[j][k] - ((A[j][i])/((A[i][i])*b));
+	        }
+	        j++;
+	        if(j==i){
+	          j++;
+	        }
+	      }
+		  }
+		}
+	}
+	
+	/* Função pivotacaoParcial
+	 * Acao: Troca a linha k pela linha i, onde ocorra
+	 *			 max{absoluto(A[i][k])}, com i em {k, ..., m}
+	 * Entrada: - Inteiro k: Passo que deve ser feita a pivotacao,
+	 *						a partir do elemento A[k][k]
+	 */
+	void pivotacaoParcial(int k){
+		int indexMaiorPivo = k;
+		for(int i=k+1; i < m; i++){
+			if(fabs(A[i][k]) > fabs(A[indexMaiorPivo][k]))
+				indexMaiorPivo = i;
+		}
+		double * aux = A[k];
+		A[k] = A[indexMaiorPivo];
+		A[indexMaiorPivo] = aux;
+		
+		permutacaoColunas[k] = indexMaiorPivo;
+		permutacaoColunas[indexMaiorPivo] = k;
+	}
+		
+	void deletaMatriz(){
+		for(int i = 0; i < m; i++){
+			if(A[i]!=nullptr)
+				delete[] A[i];
+				A[i]=nullptr;
+		}
+		delete[] A;
+	}
+	
+	/* Função alocaMatriz
+	 * Saida padrao: - True: Erro na alocacao
+	 * 							 - False: Tudo ocorreu bem
+	 */
+	bool alocaMatriz(){
+		A = (double**) malloc(m);
+		if(A!=nullptr)
+		{
+			for(int i=0; i<m; i++){
+		   	A[i] = (double*) malloc(n);
+		   	if(A[i] == nullptr){
+		   		deletaMatriz();
+		   		return true;
+				}
+			}
+			return false;
+		}
+		return true;
+	}
+	
+} Sistema;
+
+
 int main(){
   std::cout.setf( std::ios::fixed, std:: ios::floatfield );
   std::cout.precision(6);
@@ -17,75 +110,4 @@ int main(){
     cin >> op;
 	}while(op);
 
-}
-
-/* Função deletaMatriz()
- * Acao: Desaloca toda a matriz
- * Entrada: Ponteiro - Para a Matriz (double **)
- * 					Inteiro  - Quantidade de linhas
- */
-void deletaMatriz(double ** A, int m){
-	for(int i = 0; i < m; i++){
-		if(A[i]!=nullptr)
-			delete[] A[i];
-	}
-	delete[] A;
-}
-
-/* Função alocaMatriz()
- * Acao: Aloca toda a matriz
- * Entrada: Inteiros m e n, dimensoes para a matriz
- * Saida padrao: Ponteiro para a matriz mxn
- *							 Tipo (double **)
- * Saida erro:   nullptr - falta de memoria 	
- */
-double ** alocaMatriz(int m, int n){
-	double ** A = (double**) malloc(m);
-	if(A!=nullptr)
-	{
-		for(int i=0; i<m; i++){
-	   	A[i] = (double*) malloc(n);
-	   	if(A[i] == nullptr){
-	   		deletaMatriz(A, m);
-	   		break;
-			}
-		}
-	}
-	return A;
-}
-
-/* Função gauss()
- * Acao:
- * Algoritmo:
- * Entrada:
- * Saida padrao: 
- * Saida erro:
- */
-void gauss(double ** A, int m, int n){
-  for(int i=0; i < m; i++){
-    if(A[i][j] == 0){
-      for(p=0; p < m; p++){
-        if(A[p][i] != 0){
-          k=p;
-          break;
-        }
-        for(p=i-1; p < m; p++){
-          aux=A[i][p];
-          A[i][p]=A[k][p];
-           A[k][p]=aux;
-        }
-      }
-      j=0;
-      while(j < m){
-        for(k=m-1; k<n; k++){
-          b = A[i][k];
-          A[j][k] = A[j][k] - ((A[j][i])/((A[i][i])*b));
-        }
-        j++;
-        if(j==i){
-          j++;
-        }
-      }
-	  }
-	}
 }
